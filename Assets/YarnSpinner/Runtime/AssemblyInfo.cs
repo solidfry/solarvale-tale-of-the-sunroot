04586b3524796847515ebe5dd86490c4b93cb441
Elliot <ellioteweb@gmail.com>
2024-05-26 15:59:35 +1000

    added yarn spinner

---
+++ Assets/YarnSpinner/Runtime/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿/*
+Yarn Spinner is licensed to you under the terms found in the file LICENSE.md.
+*/
+
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+[assembly: AssemblyVersion("2.4.2.1")]
+[assembly: AssemblyFileVersion("2.4.2.0")]
+[assembly: AssemblyInformationalVersion("2.4.2-assetstore.1+60.Branch.assetstore.Sha.abe45d82302aea7796d77e70296e89412ee884c2")]
+[assembly: InternalsVisibleTo("YarnSpinnerTests")]
+[assembly: InternalsVisibleTo("YarnSpinner.Editor.Tests")]
+[assembly: InternalsVisibleTo("YarnSpinner.Unity.Editor")]
+
