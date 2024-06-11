da3cda26b63b87a96dd69013acac7c7f3c95ba8c
Elliot <ellioteweb@gmail.com>
2024-06-11 22:50:37 +1000

    Interaction system

---
+++ Assets/Scripts/Interaction/Interactable.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+namespace Interaction
+{
+    ///<summary>
+    /// This class is added to any GameObject that can be interacted with.
+    /// </summary>
+    public class Interactable : MonoBehaviour, IInteract
+    {
+        [SerializeField] private UnityEvent interactEvent;
+        [SerializeField] private bool isInteractable = true;
+        [SerializeField] private bool isOneTimeUse = false;
+        [SerializeField] private string interactMessage = "Interact";
+        [SerializeField] private bool showCannotInteractMessage = true;
+        [SerializeField] private string cannotInteractMessage = "Cannot interact with this object.";
+
+        public string Name => gameObject.name;
+        public string InteractMessage => interactMessage;
+
+        public void Interact()
+        {
+            if (CheckIsInteractable()) return;
+            
+            Debug.Log("Interacting with " + gameObject.name);
+            
+            if (isOneTimeUse) isInteractable = false;
+        }
+
+        private bool CheckIsInteractable()
+        {
+            if (!isInteractable)
+            {
+                if (showCannotInteractMessage)
+                    Debug.Log(cannotInteractMessage);
+
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ Assets/Scripts/Interaction/Interactor.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using Events;
+using UnityEngine;
+
+namespace Interaction
+{
+    /// <summary>
+    ///  This class is added to the player GameObject or any object to allow for interaction with other GameObjects.
+    /// </summary>
+    public class Interactor : MonoBehaviour
+    {
+        private IInteract _currentInteractable;
+        [SerializeField] LayerMask interactableLayers;
+        [SerializeField] Vector3 rayOffset;
+        [SerializeField] float interactableDistance = 3f;
+        [SerializeField] bool showDebugRay = true;
+        [SerializeField] float raycastRadius = 0.5f;
+        [SerializeField] bool sendInteractEvent = true;
+        [SerializeField] float interactCooldown = 0.1f;
+        private Coroutine _interactNullCooldown;
+        private bool _isInteracting;
+
+        private void FixedUpdate() => CheckInteractable();
+
+        private void CheckInteractable()
+        {
+            if (Physics.SphereCast(GetRayOrigin(), raycastRadius, transform.forward, out var hit, interactableDistance, interactableLayers))
+            {
+                if (hit.collider.TryGetComponent(out IInteract interactable))
+                {
+                    if (_currentInteractable != interactable)
+                    {
+                        _currentInteractable = interactable;
+                        SendInteractEvent();
+                        Debug.Log($"Interactable found: {_currentInteractable}");
+                    }
+                }
+            }
+            else
+            {
+                if (_currentInteractable != null && hit.collider == null)
+                {
+                    if (_isInteracting) return;
+
+                    if (_interactNullCooldown != null)
+                    {
+                        StopCoroutine(_interactNullCooldown);
+                    }
+
+                    _interactNullCooldown = StartCoroutine(SetInteractableNull());
+                    Debug.Log("No Interactable found");
+                }
+            }
+        }
+
+        private IEnumerator SetInteractableNull()
+        {
+            _isInteracting = true;
+            yield return new WaitForSeconds(interactCooldown);
+            _currentInteractable = null;
+            SendInteractEvent();
+            _isInteracting = false;
+        }
+
+        private void SendInteractEvent()
+        {
+            if (sendInteractEvent)
+                GlobalEvents.OnInteractableFound?.Invoke(_currentInteractable);
+        }
+
+        public void OnInteract()
+        {
+            if (_currentInteractable == null) return;
+            _currentInteractable.Interact();
+            GlobalEvents.OnInteractableUIEvent?.Invoke();
+        }
+
+        private Vector3 GetRayOrigin() => transform.position + rayOffset;
+
+        private void OnDrawGizmos()
+        {
+            if (!showDebugRay) return;
+            Gizmos.color = Color.red;
+            Gizmos.DrawRay(GetRayOrigin(), transform.forward * interactableDistance);
+            Gizmos.DrawWireSphere(GetRayOrigin() + transform.forward * interactableDistance, raycastRadius);
+
+            if (_currentInteractable != null)
+            {
+                Gizmos.color = Color.green;
+                Gizmos.DrawRay(GetRayOrigin(), transform.forward * interactableDistance);
+                Gizmos.DrawWireSphere(GetRayOrigin() + transform.forward * interactableDistance, raycastRadius);
+            }
+        }
+    }
+}
