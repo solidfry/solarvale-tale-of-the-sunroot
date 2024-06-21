aeb5dd64f5b7d8e7512f76735686ea63af4e631f
Elliot <ellioteweb@gmail.com>
2024-06-21 20:24:55 +1000

    fixed first person cam

--- Assets/Scripts/CameraSystem/CameraManager.cs
+++ Assets/Scripts/CameraSystem/CameraManager.cs
@@ -1,134 +1,137 @@
-using Cinemachine;
 using System.Collections;
+using Cinemachine;
 using UnityEngine;
-using UnityEngine.UI;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
-public class CameraManager : MonoBehaviour
+namespace CameraSystem
 {
-    [Header("Photo Taker")]
-    [SerializeField] private Image photoDisplayArea;
-    [SerializeField] private GameObject photoFrame;
-
-    [Header("Hide all UI")]
-    [SerializeField] private GameObject[] hideUI;
-    [SerializeField] private GameObject[] takePhotoUI;
-
-    private Texture2D screenCapture;
-    private bool viewingPhoto;
-
-    public InputActionAsset inputActionAsset; // Reference to the Input Action Asset
-    private InputAction takePhotoAction;
-
-    public CinemachineVirtualCamera thirdPersonCamera;
-    public CinemachineVirtualCamera firstPersonCamera;
-
-    void Awake()
+    public class CameraManager : MonoBehaviour
     {
-        var playerActionMap = inputActionAsset.FindActionMap("Player");
-        takePhotoAction = playerActionMap.FindAction("TakePhoto");
-    }
+        [Header("Photo Taker")]
+        [SerializeField] private Image photoDisplayArea;
+        [SerializeField] private GameObject photoFrame;
 
-    void OnEnable()
-    {
-        takePhotoAction.Enable();
-        takePhotoAction.performed += OnTakePhoto;
-    }
+        [Header("Hide all UI")]
+        [SerializeField] private GameObject[] hideUI;
+        [SerializeField] private GameObject[] takePhotoUI;
 
-    void OnDisable()
-    {
-        takePhotoAction.performed -= OnTakePhoto;
-        takePhotoAction.Disable();
-    }
+        private Texture2D screenCapture;
+        private bool viewingPhoto;
 
-    void OnTakePhoto(InputAction.CallbackContext context)
-    {
-        Debug.Log("OnTakePhoto: Photo action performed");
-        if (IsFirstPersonCameraHigherPriority())
+        public InputActionAsset inputActionAsset; // Reference to the Input Action Asset
+        private InputAction takePhotoAction;
+
+        public CinemachineVirtualCamera thirdPersonCamera;
+        public CinemachineVirtualCamera firstPersonCamera;
+
+        void Awake()
         {
-            StartCoroutine(CapturePhoto());
-        }
-        else
-        {
-            Debug.Log("OnTakePhoto: First person camera does not have higher priority");
-        }
-    }
-
-    public bool IsFirstPersonCameraHigherPriority()
-    {
-        bool isFirstHigher = firstPersonCamera.Priority > thirdPersonCamera.Priority;
-        Debug.Log($"IsFirstPersonCameraHigherPriority: {isFirstHigher}");
-        return isFirstHigher;
-    }
-
-
-    IEnumerator CapturePhoto()
-    {
-        Debug.Log("CapturePhoto: Starting photo capture coroutine");
-
-        // Hide UI elements
-        foreach (var ui in hideUI)
-        {
-            ui.SetActive(false);
+            var playerActionMap = inputActionAsset.FindActionMap("Player");
+            takePhotoAction = playerActionMap.FindAction("TakePhoto");
         }
 
-        viewingPhoto = true;
-
-        yield return new WaitForEndOfFrame();
-
-        Rect regionToRead = new Rect(0, 0, Screen.width, Screen.height);
-
-        // Use RGBA32 format for screen capture
-        screenCapture = new Texture2D(Screen.width, Screen.height, TextureFormat.RGBA32, false);
-        screenCapture.ReadPixels(regionToRead, 0, 0, false);
-        screenCapture.Apply();
-
-        // Display the photo
-        ShowPhoto();
-
-        yield return new WaitForSeconds(1f); // Cooldown period
-
-        // Remove the photo and reset UI elements
-        RemovePhoto();
-
-        // Show UI elements again
-        foreach (var ui in hideUI)
+        void OnEnable()
         {
-            ui.SetActive(true);
-        }
-    }
-
-    void ShowPhoto()
-    {
-        if (screenCapture != null)
-        {
-            Sprite photoSprite = Sprite.Create(screenCapture, new Rect(0.0f, 0.0f, screenCapture.width, screenCapture.height), new Vector2(0.5f, 0.5f), 100.0f);
-            photoDisplayArea.sprite = photoSprite;
-
-            // Show the photo frame
-            photoFrame.SetActive(true);
-        }
-        else
-        {
-            Debug.LogError("Screen capture texture is null");
-        }
-    }
-
-    void RemovePhoto()
-    {
-        // Hide take photo UI elements
-        foreach (var ui in takePhotoUI)
-        {
-            ui.SetActive(false);
+            takePhotoAction.Enable();
+            takePhotoAction.performed += OnTakePhoto;
         }
 
-        viewingPhoto = false;
+        void OnDisable()
+        {
+            takePhotoAction.performed -= OnTakePhoto;
+            takePhotoAction.Disable();
+        }
 
-        // Hide the photo frame
-        photoFrame.SetActive(false);
+        void OnTakePhoto(InputAction.CallbackContext context)
+        {
+            Debug.Log("OnTakePhoto: Photo action performed");
+            if (IsFirstPersonCameraHigherPriority())
+            {
+                StartCoroutine(CapturePhoto());
+            }
+            else
+            {
+                Debug.Log("OnTakePhoto: First person camera does not have higher priority");
+            }
+        }
 
-        // Clear the photo display area
-        photoDisplayArea.sprite = null;
+        public bool IsFirstPersonCameraHigherPriority()
+        {
+            bool isFirstHigher = firstPersonCamera.Priority > thirdPersonCamera.Priority;
+            Debug.Log($"IsFirstPersonCameraHigherPriority: {isFirstHigher}");
+            return isFirstHigher;
+        }
+
+
+        IEnumerator CapturePhoto()
+        {
+            Debug.Log("CapturePhoto: Starting photo capture coroutine");
+
+            // Hide UI elements
+            foreach (var ui in hideUI)
+            {
+                ui.SetActive(false);
+            }
+
+            viewingPhoto = true;
+
+            yield return new WaitForEndOfFrame();
+
+            Rect regionToRead = new Rect(0, 0, Screen.width, Screen.height);
+
+            // Use RGBA32 format for screen capture
+            screenCapture = new Texture2D(Screen.width, Screen.height, TextureFormat.RGBA32, false);
+            screenCapture.ReadPixels(regionToRead, 0, 0, false);
+            screenCapture.Apply();
+
+            // Display the photo
+            ShowPhoto();
+
+            yield return new WaitForSeconds(1f); // Cooldown period
+
+            // Remove the photo and reset UI elements
+            RemovePhoto();
+
+            // Show UI elements again
+            foreach (var ui in hideUI)
+            {
+                ui.SetActive(true);
+            }
+        }
+
+        void ShowPhoto()
+        {
+            if (screenCapture != null)
+            {
+                Sprite photoSprite = Sprite.Create(screenCapture, new Rect(0.0f, 0.0f, screenCapture.width, screenCapture.height), new Vector2(0.5f, 0.5f), 100.0f);
+                photoDisplayArea.sprite = photoSprite;
+
+                // Show the photo frame
+                photoFrame.SetActive(true);
+            }
+            else
+            {
+                Debug.LogError("Screen capture texture is null");
+            }
+        }
+
+        void RemovePhoto()
+        {
+            // Hide take photo UI elements
+            foreach (var ui in takePhotoUI)
+            {
+                ui.SetActive(false);
+            }
+
+            viewingPhoto = false;
+
+            // Hide the photo frame
+            photoFrame.SetActive(false);
+
+            // Clear the photo display area
+            photoDisplayArea.sprite = null;
+        }
     }
 }
 
--- Assets/Scripts/CameraSystem/PhotographyManager.cs
+++ Assets/Scripts/CameraSystem/PhotographyManager.cs
@@ -1,54 +1,55 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 using Cinemachine;
+using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class PhotographyManager : MonoBehaviour
+namespace CameraSystem
 {
-    public CinemachineVirtualCamera thirdPersonCamera;
-    public CinemachineVirtualCamera firstPersonCamera;
-    public InputActionAsset inputActionAsset; 
-
-
-    private InputAction cameraOpenAction;
-
-    void Awake()
+    public class PhotographyManager : MonoBehaviour
     {
-        var playerActionMap = inputActionAsset.FindActionMap("Player");
-        cameraOpenAction = playerActionMap.FindAction("CameraOpen");
-    }
+        public CinemachineVirtualCamera thirdPersonCamera;
+        public CinemachineVirtualCamera firstPersonCamera;
+        public InputActionAsset inputActionAsset; 
 
-    void OnEnable()
-    {
-        cameraOpenAction.Enable();
-        cameraOpenAction.performed += OnCameraOpen;
-    }
 
-    void OnDisable()
-    {
-        cameraOpenAction.performed -= OnCameraOpen;
-        cameraOpenAction.Disable();
-    }
+        private InputAction cameraOpenAction;
 
-    void OnCameraOpen(InputAction.CallbackContext context)
-    {
-        SwitchCamera();
-    }
-
-    void SwitchCamera()
-    {
-        if (thirdPersonCamera.Priority > firstPersonCamera.Priority)
+        void Awake()
         {
-            thirdPersonCamera.Priority = 0;
-            firstPersonCamera.Priority = 1;
+            var playerActionMap = inputActionAsset.FindActionMap("Player");
+            cameraOpenAction = playerActionMap.FindAction("CameraOpen");
         }
-        else
+
+        void OnEnable()
         {
-            thirdPersonCamera.Priority = 1;
-            firstPersonCamera.Priority = 0;
+            cameraOpenAction.Enable();
+            cameraOpenAction.performed += OnCameraOpen;
         }
+
+        void OnDisable()
+        {
+            cameraOpenAction.performed -= OnCameraOpen;
+            cameraOpenAction.Disable();
+        }
+
+        void OnCameraOpen(InputAction.CallbackContext context)
+        {
+            SwitchCamera();
+        }
+
+        void SwitchCamera()
+        {
+            if (thirdPersonCamera.Priority > firstPersonCamera.Priority)
+            {
+                thirdPersonCamera.Priority = 0;
+                firstPersonCamera.Priority = 1;
+            }
+            else
+            {
+                thirdPersonCamera.Priority = 1;
+                firstPersonCamera.Priority = 0;
+            }
+        }
+
+
     }
-
-
 }
