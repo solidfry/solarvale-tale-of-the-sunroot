82cdf517aa6d7cf55b8acf684d5d98034f49cefa
Elliot <ellioteweb@gmail.com>
2024-06-22 22:04:24 +1000

    Added button links to game and quit from main menu

--- Assets/Scripts/Utilities/SceneLoadHelper.cs
+++ Assets/Scripts/Utilities/SceneLoadHelper.cs
@@ -15,5 +15,47 @@ namespace Utilities
                 Debug.LogError($"Scene {sceneName.ToString()} does not exist.");
             }
         } 
+        
+        public static void LoadScene(int sceneIndex)
+        {
+            if (Application.CanStreamedLevelBeLoaded(sceneIndex))
+            {
+                UnityEngine.SceneManagement.SceneManager.LoadScene(sceneIndex);
+            }
+            else
+            {
+                Debug.LogError($"Scene {sceneIndex.ToString()} does not exist.");
+            }
+        }
+        
+        public static void LoadSceneAdditive(string sceneName)
+        {
+            if (Application.CanStreamedLevelBeLoaded(sceneName))
+            {
+                UnityEngine.SceneManagement.SceneManager.LoadScene(sceneName, UnityEngine.SceneManagement.LoadSceneMode.Additive);
+            }
+            else
+            {
+                Debug.LogError($"Scene {sceneName.ToString()} does not exist.");
+            }
+        }
+        
+        public static void LoadSceneAdditive(int sceneIndex)
+        {
+            if (Application.CanStreamedLevelBeLoaded(sceneIndex))
+            {
+                UnityEngine.SceneManagement.SceneManager.LoadScene(sceneIndex, UnityEngine.SceneManagement.LoadSceneMode.Additive);
+            }
+            else
+            {
+                Debug.LogError($"Scene {sceneIndex.ToString()} does not exist.");
+            }
+        }
+
+        public static void QuitApplication()
+        {
+            Application.Quit();
+        }
+
     }
 }
