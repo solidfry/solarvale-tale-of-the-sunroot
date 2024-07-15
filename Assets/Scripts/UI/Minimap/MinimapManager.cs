cbb4ae046794fad2cc0b030d51215d267d5058de
Ay3e <79293475+Ay3e@users.noreply.github.com>
2024-07-15 18:25:46 +1000

    Made a function to unlock the minimap
    This minimap will help players navigate with quests

--- Assets/Scripts/UI/Minimap/MinimapManager.cs
+++ Assets/Scripts/UI/Minimap/MinimapManager.cs
@@ -7,12 +7,31 @@ public class MinimapManager : MonoBehaviour
     [SerializeField] private GameObject playerCharacter;
     [SerializeField] private Transform mainCamera;
 
+    [SerializeField] private GameObject miniMap;
+
+    [SerializeField] private Animator miniMapBoarderAnimator;
+    [SerializeField] private Animator miniMapImageAnimator;
+
+
+    private void Start()
+    {
+        miniMap.SetActive(false);
+    }
+
     private void LateUpdate()
     {
         transform.position = new Vector3(playerCharacter.transform.position.x, 400, playerCharacter.transform.position.z);
 
         Vector3 rotation = new Vector3(90, mainCamera.eulerAngles.y, 0);
         transform.rotation = Quaternion.Euler(rotation);
+    }
+
+    public void UnlockMiniMap()
+    {
+        miniMap.SetActive(true);
+        miniMapBoarderAnimator.SetBool("MinimapUnlocked", true);
+        miniMapImageAnimator.SetBool("MinimapUnlocked", true);
+
     }
 
 }
