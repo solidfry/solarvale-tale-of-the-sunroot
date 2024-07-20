500b562d67e91f30043d837f1f3c2972fa14caba
Ay3e <79293475+Ay3e@users.noreply.github.com>
2024-07-20 17:37:04 +1000

    Optimising minimap

---
+++ Assets/Scripts/UI/Minimap/MinimapIcon.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class MinimapIcon : MonoBehaviour
+{
+    public Image Image;
+    public RectTransform RectTransform;
+    public RectTransform IconRectTransform;
+}
---
+++ Assets/Scripts/UI/Minimap/MinimapWorldObject.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MinimapWorldObject : MonoBehaviour
+{
+    [SerializeField] private bool followObject = false;
+    [SerializeField] private Sprite minimapIcon;
+
+    public Sprite MinimapIcon => minimapIcon;
+
+    private void Start()
+    {
+        MinimapController.Instance.RegisterMinimapWorldObject(this, followObject);
+    }
+
+    private void OnDestroy()
+    {
+        MinimapController.Instance.RemoveMinimapWorldObject(this);    
+    }
+}
