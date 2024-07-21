94cd5c0cc51c9148017bce66aabb3abb46d686c1
Ay3e <79293475+Ay3e@users.noreply.github.com>
2024-07-21 20:44:00 +1000

    Fixed causes of minimap and bugs
    fixes include:
    - allowing only the player to trigger quest events
    - only having quest area dialogue play once

--- Assets/Scripts/DialogueSystem/ActivateDialogueOnTrigger.cs
+++ Assets/Scripts/DialogueSystem/ActivateDialogueOnTrigger.cs
@@ -6,9 +6,18 @@ using UnityEngine.Events;
 public class ActivateDialogueOnTrigger : MonoBehaviour
 {
     public UnityEvent onTriggerEvent;
+    private bool _hasBeenTriggered = false;
 
     private void OnTriggerEnter(Collider other)
     {
-        onTriggerEvent?.Invoke();
+        if (other.gameObject.layer == LayerMask.NameToLayer("Player") && other.CompareTag("Player"))
+        {
+            if (!_hasBeenTriggered)
+            {
+                onTriggerEvent?.Invoke();
+                _hasBeenTriggered = true;
+            }
+
+        }
     }
 }
