3e145e26a18af3758667bc40a5a56b43261ed2b3
Ay3e <79293475+Ay3e@users.noreply.github.com>
2024-07-23 12:01:15 +1000

    QuestIcon Implemented into world scene
    Changed the camera to be orthographic

--- Assets/Scripts/UI/Onboarding/QuestUIManager.cs
+++ Assets/Scripts/UI/Onboarding/QuestUIManager.cs
@@ -6,17 +6,53 @@ using UnityEngine;
 public class QuestUIManager : MonoBehaviour
 {
     [SerializeField] private GameObject playerCharacter;
-    [SerializeField] private GameObject questUIIcon;
-    void Update()
+    [SerializeField] private SpriteRenderer questUIIcon;
+
+    private bool _isInTrigger;
+
+    private void Start()
+    {
+        questUIIcon.enabled = true;
+    }
+
+    private void Update()
+    {
+        if (_isInTrigger)
+        {
+            questUIIcon.enabled = true;
+            QuestIconLockedOnPlayer();
+        }
+        else
+        {
+            questUIIcon.enabled = false;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Player") && other.CompareTag("Player"))
+        {
+            _isInTrigger = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.layer == LayerMask.NameToLayer("Player") && other.CompareTag("Player"))
+        {
+            _isInTrigger = false;
+        }
+    }
+
+    private void QuestIconLockedOnPlayer()
     {
         Vector3 directionToFace = playerCharacter.transform.position - questUIIcon.transform.position;
-
-        directionToFace.y = 0;
+        directionToFace.y = 0; // Keep the direction horizontal
 
         Quaternion lookRotation = Quaternion.LookRotation(directionToFace);
         questUIIcon.transform.rotation = lookRotation;
-        questUIIcon.transform.rotation = Quaternion.Euler(questUIIcon.transform.rotation.eulerAngles.x,
-                                                          questUIIcon.transform.rotation.eulerAngles.y,
-                                                          -45);
+
+        // Add the tilt (assuming you want to tilt it on the z-axis by -45 degrees)
+        questUIIcon.transform.Rotate(0, 0, -45);
     }
 }
