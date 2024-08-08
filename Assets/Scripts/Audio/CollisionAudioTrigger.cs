2651c2773844a6999c9199e0b6bb18d6aa5db5fe
Ay3e <79293475+Ay3e@users.noreply.github.com>
2024-08-08 19:41:37 +1000

    Implemented cricket audio

---
+++ Assets/Scripts/Audio/CollisionAudioTrigger.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollisionAudioTrigger : MonoBehaviour
+{
+    private AkAmbient akAmbient;
+    private float fadeOutDuration = 2.0f;
+    [SerializeField] private string wwiseEventName;
+
+    private Coroutine playSoundCoroutine;
+
+    private void Start()
+    {
+        akAmbient = GetComponent<AkAmbient>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            // Start the coroutine to play sound at random intervals between 0.75 and 2 seconds
+            playSoundCoroutine = StartCoroutine(PlaySoundAtIntervals());
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            // Stop the coroutine to cease playing sounds
+            if (playSoundCoroutine != null)
+            {
+                StopCoroutine(playSoundCoroutine);
+                playSoundCoroutine = null;
+            }
+
+            // Fade out the currently playing sound
+            AkSoundEngine.ExecuteActionOnEvent(wwiseEventName, AkActionOnEventType.AkActionOnEventType_Stop, gameObject, (int)(fadeOutDuration * 1000), AkCurveInterpolation.AkCurveInterpolation_Linear);
+        }
+    }
+
+    private IEnumerator PlaySoundAtIntervals()
+    {
+        while (true) // Loop continuously until the coroutine is stopped
+        {
+            // Play the ambient sound
+            akAmbient.HandleEvent(gameObject);
+
+            // Wait for a random time between 0.75 and 2 seconds
+            float waitTime = Random.Range(0.75f, 2.0f);
+            yield return new WaitForSeconds(waitTime);
+        }
+    }
+}
