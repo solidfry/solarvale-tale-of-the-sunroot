5f1c43df8d799099d277e1d5c63a4445305fc0a7
Elliot <ellioteweb@gmail.com>
2024-08-10 16:39:55 +1000

    Updated mountainash for better perf

--- Assets/Scripts/Player/PlayerGrassEffector.cs
+++ Assets/Scripts/Player/PlayerGrassEffector.cs
@@ -4,7 +4,12 @@ namespace Player
 {
     public class PlayerGrassEffector : MonoBehaviour
     {
+        [SerializeField] float additionalOffset = 0.5f;
+        
         CharacterController _characterController;
+        private static readonly int PlayerPosition = Shader.PropertyToID("_PlayerPosition");
+
+        private float _offset;
         private void Awake()
         { 
             _characterController = GetComponent<CharacterController>();
@@ -14,7 +19,8 @@ namespace Player
         void Update()
         {
             if (_characterController == null) return;
-            Shader.SetGlobalVector("_PlayerPosition", transform.position + Vector3.up *  _characterController.radius);
+            _offset = _characterController.radius + additionalOffset;
+            Shader.SetGlobalVector(PlayerPosition, transform.position + Vector3.up * _offset);
         }
     }
 }
